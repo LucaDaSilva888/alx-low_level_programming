a57bf3ea8ecff814f47c75a6005bd9b32a84536c
LucaDaSilva888 <lucadasilva888@gmail.com>
2022-06-02 10:22:39 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
-#include <studio.h>
+#include <stdio.h>
 
 /**
 * main - Entry point
  * puts message
  * Return: 0
- * /
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");return (0);
