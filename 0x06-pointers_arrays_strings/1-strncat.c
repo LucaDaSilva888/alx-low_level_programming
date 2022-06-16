61e2205dff2371a796a52f71b54e8e88e9024155
LucaDaSilva888 <lucadasilva888@gmail.com>
2022-06-16 10:55:20 -0700

    task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_strncat - function that concatenates two strings. it use at most n bytes from src. src does not need to be null terminated.
+ *_strncat - function that concatenates two strings. it will use * at most n bytes from src. src does not need to be null terminated.
  *@dest: pointer to destination string.
  *@src: pointer to source string.
  *@n: number of bytes to be concatenated.
@@ -9,7 +9,7 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int length j;
+	int length, j;
 	/* j is a counter for  n bytes of src to be concatenated */
 	/* length = length of destination string */
 
